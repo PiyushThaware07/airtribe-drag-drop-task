dc4a7c5ac32dcd17f730a037787fd17c88079580
Piyush Thaware <Piyushbthaware@gmail.com>
2024-09-03 15:27:47 +0530

    Add : new column add details

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from "react-router-dom";
 import { HiPlus } from "react-icons/hi";
 import initialData from "../json/data.json";
@@ -41,42 +41,68 @@ export default function Container() {
     };
 
 
+    // ADD new column
+    /*
+    const inputRef = useRef(null);
+    function handleAddColumn(event) {
+        if (event.key === "Enter" && inputRef.current.value.trim() !== "") {
+            const key = inputRef.current.value
+            setData({
+                ...data,
+                [key]: []
+            })
+        }
+    }
+        */
+
+
     return (
-        <div className='app_container grid grid-cols-1 md:grid-cols-3 gap-10'>
-            {
-                Object.keys(data).map((status) => {
-                    return (
-                        <section key={status}
-                            onDragOver={(e) => e.preventDefault()}
-                            onDrop={(e) => handleDropElement(e, status)}>
-                            <div className="flex flex-nowrap items-center justify-between gap-5 mb-5">
-                                <div className="text-xs font-semibold flex flex-nowrap items-center gap-4">
-                                    <h1 className={`
+
+        <>
+            {/* <div className="max-w-sm mx-auto mb-20">
+                <input type="text" placeholder='Column Name' ref={inputRef} onKeyDown={handleAddColumn} className='text-sm font-medium p-2 focus:outline-none border rounded w-full' />
+            </div> */}
+            <div className='app_container grid grid-cols-1 md:grid-cols-3 gap-10'>
+                {
+                    Object.keys(data).map((status) => {
+                        return (
+                            <section key={status}
+                                onDragOver={(e) => e.preventDefault()}
+                                onDrop={(e) => handleDropElement(e, status)}>
+                                <div className="flex flex-nowrap items-center justify-between gap-5 mb-5">
+                                    <div className="text-xs font-semibold flex flex-nowrap items-center gap-4">
+                                        <h1 className={`
                                         ${status === "not started" && "bg-red-200 text-red-600 border-red-600 "}
                                         ${status === "in progress" && "bg-yellow-200 text-yellow-600 border-yellow-600"}
                                         ${status === "completed" && "bg-green-200 text-green-600 border-green-600"} 
                                         inline capitalize px-3 py-1 rounded border`}>{status}</h1>
-                                    <h1 className='text-gray-400 text-[16px] font-medium'>{data[status].length}</h1>
+                                        <h1 className='text-gray-400 text-[16px] font-medium'>{data[status].length}</h1>
+                                    </div>
+                                    <HiPlus className='text-xl text-gray-400' />
                                 </div>
-                                <HiPlus className='text-xl text-gray-400' />
-                            </div>
-                            <ul className='flex flex-nowrap flex-col gap-4 mt-10'>
-                                {data[status].map((task) => (
-                                    <li
-                                        key={task.title}
-                                        className='text-xs tracking-[1px] font-medium text-gray-500 bg-white rounded border p-3 select-none cursor-pointer'
-                                        draggable
-                                        onDragStart={(e) => handleDragStart(e, task, status)}
-                                        onClick={() => navigate("/editable", { state: {task:task,status:status} })}>
-                                        {task.title}
-                                    </li>
-                                ))}
-                            </ul>
-                            <AddElement status={status} addTask={addTask} />
-                        </section>
-                    )
-                })
-            }
-        </div>
+                                <ul className='flex flex-nowrap flex-col gap-4 mt-10'>
+                                    {data[status].map((task) => (
+                                        <li
+                                            key={task.title}
+                                            className='text-xs tracking-[1px] font-medium text-gray-500 bg-white rounded border p-3 select-none cursor-pointer'
+                                            draggable
+                                            onDragStart={(e) => handleDragStart(e, task, status)}
+                                            onClick={() => {
+                                                navigate("/editable", { state: { task: task, status: status } })
+                                            }}>
+                                            <span>
+                                                {task.title}
+                                            </span>
+                                        </li>
+                                    ))}
+                                </ul>
+                                <AddElement status={status} addTask={addTask} />
+                            </section>
+                        )
+                    })
+                }
+            </div>
+        </>
+
     )
 }
