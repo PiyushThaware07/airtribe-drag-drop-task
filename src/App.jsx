e0110769e4ff1d42aa1c2fe346334b7421a0766a
Piyush Thaware <Piyushbthaware@gmail.com>
2024-09-03 15:32:46 +0530

    Add : new column add details

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Editable from './components/Editable';
 export default function App() {
   return (
     <main className="min-h-screen bg-gray-100">
-      <div className="max-w-6xl h-full mx-auto py-10  px-5 lg:px-0">
+      <div className="max-w-6xl h-full mx-auto py-10">
         <Routes>
           <Route path="/editable" element={<Editable />} />
           <Route path="/" element={<Container />} />
